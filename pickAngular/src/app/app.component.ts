a5bb4d67c0879180d93ad16ffa4e26794f31efa3
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-07-24 18:13:11 -0300

    Add WebAPI

--- pickAngular/src/app/app.component.ts
+++ pickAngular/src/app/app.component.ts
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Aplicação Angular - Pick';
-  description = 'Lion';
-  urlImage = 'https://thumbs.dreamstime.com/b/wildlife-male-african-lion-sitting-alone-field-58565882.jpg';
+  photos = [
+    {
+      url: "http://davemosher.com/blog/wp-content/uploads/2011/11/sombrero-galaxy-200x200.jpg",
+      description: "Andromeda Galaxy"
+    },
+    {
+      url:"https://c.slashgear.com/wp-content/uploads/2015/09/andromeda-galaxy-1-200x200.jpg",
+      description: "Milky Way Galaxy"
+    },
+    {
+      url: "http://dlandroid.com/wp-content/uploads/2017/07/Galaxy-Pack-3-200x200.jpg",
+      description: "Tatatoo Galaxy"
+    },
+    {
+      url: "https://avatarfiles.alphacoders.com/973/thumb-97346.jpg",
+      description: "Sfaeg Galaxy"
+    }
+  ]
 }
